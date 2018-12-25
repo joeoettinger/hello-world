601be44f029cd3e02289f80e9e8640b60cab9b39
joeoettinger <joeoettinger@gmail.com>
2018-12-25 13:55:49 -0500

    Add files via upload

---
+++ helloWorld_3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3.5
+# helloWorld_3.py 2009-11-12 joseph t oettinger
+# purpose: show how python3.x prints "hello, world"
+# archive: contact joeoettinger@netscape.net; in /abstxt 
+# comment: 
+# revision log
+from __future__ import absolute_import, division, print_function
+
+print("hello, world")
+
